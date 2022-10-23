2135ed9e0fd335bbf7f30330d72bb4a8dea86bc1
mys1024 <mys.1024@outlook.com>
2022-10-23 16:18:53 +0800

    feat: new site `https://chatroom.1024.cab`

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@
       <a class="link" href="https://short.1024.cab">https://short.1024.cab</a>
       <br>
       <a class="link" href="https://tixy.1024.cab">https://tixy.1024.cab</a>
+      <br>
+      <a class="link" href="https://chatroom.1024.cab">https://chatroom.1024.cab</a>
     </p>
   </div>
 </template>
